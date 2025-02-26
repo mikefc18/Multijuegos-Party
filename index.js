901c8a11c29bf47d8d0c2d6c3f48b3bd317fcc03
Mikefc <miguelroaguilera@gmail.com>
2025-02-26 21:31:42 +0100

    Agregados cambios de configuración para el despliegue

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;  // Usa el puerto de la variable de entor
 
 // Recupera la API Key de Gemini desde las variables de entorno
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const GEMINI_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 // Habilita CORS para que el servidor pueda manejar solicitudes de otros dominios
 app.use(cors());
